346e3fa0e973845369cbce8352ab07e756378202
vignesh <vignesh@localhost.localdomain>
2019-11-29 12:47:01 +0530

    Microservice End Points Coding Compeleted

--- service/routes.js
+++ service/routes.js
@@ -2,6 +2,13 @@ const app = require('express').Router();
 
 const util = require('./util');
 app.post('/createtask', util.createtask);
+app.post('/gettask', util.gettask);
+app.post('/updatetask', util.updatetask);
+app.post('/updatetaskstatus', util.updatetaskstatus);
+app.post('/createtimesheet', util.createtimesheet);
+app.post('/updatetimesheet', util.updatetimesheet);
+app.post('/gettaskreport', util.gettaskreport);
+app.post('/gettimesheetreport', util.gettimesheetreport);
 
 /* Default */
 app.get('/', (req, res) => {
